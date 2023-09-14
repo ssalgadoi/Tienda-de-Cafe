495873548779ea25cc0ebcc132d5d2038077d788
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-14 16:42:56 -0300

    Se agrega primera parte del Def Categry

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def blog(request):
     posts = Post.objects.all()
     return render(request, "blog/blog.html", {'posts': posts})
+
+def category(recuest, category_id):
+    category = Ctegory.objects.get(id=catgory_id)
+    return render (request, "blog/category.html", { 'category': category })
