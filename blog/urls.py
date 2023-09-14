54309d0cf687442cd04cd37cd238c72403d1ecc5
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-14 16:42:27 -0300

    Se agrega la ruta de categoria

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name="blog"),
+    # le entregamos la ruta de la categoria y el dinamismo a la url
+    path('category/<int:category_id>/', views.category, name="category")
 ]
