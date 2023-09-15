eb0cf6cba30fd13a10bc6268e6153d14b7a4f3dc
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-15 13:16:42 -0300

    Se habia actualizado buscando el error

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name="blog"),
-    # le entregamos la ruta de la categoria y el dinamismo a la url
-    path('category/<int:category_id>/', views.category, name="category")
+    path('category/<int:category_id>/', views.category, name="category"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 
 # Create your views here.
+
 def blog(request):
     posts = Post.objects.all()
-    return render(request, "blog/blog.html", {'posts': posts})
+    return render(request, "blog/blog.html", {'posts':posts})
 
-def category(recuest, category_id):
-    category = Ctegory.objects.get(id=catgory_id)
-    return render (request, "blog/category.html", { 'category': category })
+def category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    return render(request, "blog/category.html", {'category':category})
