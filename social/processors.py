9a9db4687f8e0114db5db8849bf4e17e33a5a54f
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-15 20:12:25 -0300

    Se guarda pero hay un error

--- social/processors.py
+++ social/processors.py
@@ -1,4 +1,4 @@
-from .models import Link 
+
 
 # En resumen, este código crea un diccionario que mapea nombres clave de enlaces a sus 
 # respectivos URLs. Puede ser útil para proporcionar enlaces en una plantilla HTML para 
@@ -6,8 +6,10 @@ from .models import Link
 # y el URL "https://www.facebook.com", podrás acceder a ese enlace utilizando ctx['facebook'], 
 # que te devolverá "https://www.facebook.com".
 
+from .models import Link
+
 def ctx_dict(request):
-    ctx : {}
+    ctx = {}
     links = Link.objects.all()
     for link in links:
         ctx[link.key] = link.url
