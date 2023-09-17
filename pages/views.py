dd0aef3980657fd4e97329889e22c21edf47479a
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-17 03:07:42 -0300

    Se trabaja pageapp

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Page
-# Create your views here.
 
 def page(request, page_id):
-    page = get_list_or_404(Page, id=page_id)
-    return render(request, 'page/sample.html', { 'page': page })
+    page = get_object_or_404(Page, id=page_id)
+    return render(request, "page/sample.html", { 'page': page })
