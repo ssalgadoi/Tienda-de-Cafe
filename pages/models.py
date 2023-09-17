9f447f9a0fd7ded7e3098280717fc5411f55716b
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-17 02:05:51 -0300

    Se crea el modelo de page

---
+++ pages/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(verbose_name="Título", max_length=200)
+    content = models.TextField(verbose_name="Contemido")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de Edición")
+    
+    class Meta:
+        verbose_name = "pagina"#Cambia los nombres en el admina español
+        verbose_name_plural = "paginas"
+        ordering = ['title']#Se ordenan los proyectos de los ultimos subidos
+        
+    def __str__(self):
+        return self.title
