82362a9f5e5bd4119296d388234ac3f49bd9b817
salgadoIbacache <se.salgado@duocuc.cl>
2023-09-19 15:15:59 -0300

    Correguimos category.html

--- blog/models.py
+++ blog/models.py
@@ -35,7 +35,7 @@ class Post(models.Model):
     published = models.DateTimeField(verbose_name="Fecha de publicacion", default=now)
     image = models.ImageField(verbose_name="Imagen", upload_to="blog", null=True, blank=True)
     author = models.ForeignKey(User, verbose_name="Autor", on_delete=models.CASCADE)
-    categories = models.ManyToManyField(Category, verbose_name="Categorías", related_name="get_post")
+    categories = models.ManyToManyField(Category, verbose_name="Categorías", related_name="get_posts")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de Edición")
     
